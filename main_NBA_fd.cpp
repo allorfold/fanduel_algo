1b1f3c5a1afa7b597ca6b9182b7a646d3924588a
Dave Saksa <goallinorfold@gmail.com>
2015-01-19 09:57:18 -0500

    Adding cpp file

---
+++ main_NBA_fd.cpp
@@ -0,0 +1,40 @@
+/* 
+ * File:   main_NBA_fd.cpp
+ * Author: David Saksa
+ *
+ * Created on November 27, 2014, 10:09 AM
+ */
+
+
+ /*	THIS IS THE MAIN FILE FOR THE NBA ALGORITHM
+
+	Each of the functions are defined in the player_proj_nba.cpp file
+	and defined in the header file as well.
+
+	TO BE ADDED:
+
+	Create a function to pull information off of a website instead of using a text file.
+
+ */
+
+
+// Using the following includes
+
+#include <cstdlib>
+#include "player_proj_nba.h"
+#include<map>
+using std::map;
+#include<iostream>
+using std::cout; using std::endl; using std::cin;
+#include<fstream>
+using std::ifstream;
+#include<vector>
+using std::vector;
+#include<algorithm>
+using std::find; using std::copy;
+#include<iterator>
+using std::ostream_iterator;
+using std::istringstream;
+using std::istream_iterator;
+
+
